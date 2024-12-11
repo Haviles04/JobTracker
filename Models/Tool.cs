be2d05f7501e506a0c25c1b4121072f1f03faad4
haviles04 <haviles04@gmail.com>
2024-12-11 16:32:10 -0500

    feat: added property NeedsRepair and defaulted to false

--- Models/Tool.cs
+++ Models/Tool.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace JobTracker.Models
 {
     public class Tool
     {
         public long Id { get; set; }
-        [Required]
         [StringLength(50)]
         public required string Name { get; set; }
+        [DefaultValue(false)]
+        public required bool NeedsRepair {get; set;}
     }
 }
