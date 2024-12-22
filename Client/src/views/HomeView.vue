68c78e5319c37ade0f0817d2c8781126886b5ad8
haviles04 <haviles04@gmail.com>
2024-12-22 14:29:34 -0500

    feat: added login view and logic

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+const title: string = "Welcome to Job Tracker";
 </script>
 
 <template>
-  <main class="w-full h-full flex justify-center">
-    <h1 class="text-2xl">Job Tracker</h1>
+  <main>
+    <div>
+      <h1>{{title}}</h1>
+    </div>
   </main>
 </template>
