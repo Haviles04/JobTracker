2c523f388d2fb071cf682e1ef46b1347c9904c8a
haviles04 <haviles04@gmail.com>
2024-12-22 11:59:02 -0500

    feat: added tailwind

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-full h-full flex justify-center">
+    <h1 class="text-2xl">Job Tracker</h1>
   </main>
 </template>
